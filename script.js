6783b3023be3dbbd185668537c747eadf51a6962
krekerkill <Semen_Gruzdev@mail.ru>
2025-03-27 19:46:10 +0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function selectBrand(brand) {
     if (window.Telegram && Telegram.WebApp) {
-        // Отправка данных в Telegram-бота
         Telegram.WebApp.sendData(JSON.stringify({
             action: "select_brand",
             brand: brand
         }));
     } else {
-        console.log("Выбран бренд:", brand); // Для теста вне Telegram
+        console.log("Выбран бренд:", brand);
     }
 }
